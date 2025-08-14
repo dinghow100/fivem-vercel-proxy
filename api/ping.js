564913935d38fb207d24a2c16c82f0bf505ebd5f
dinghow100 <65144064+dinghow100@users.noreply.github.com>
2025-08-14 18:09:18 +1000

    Delete api/ping.js

--- api/ping.js
+++
@@ -1,7 +0,0 @@
-export const runtime = "edge";
-export default async function handler() {
-  console.log("[ping] hit", Date.now());
-  return new Response(JSON.stringify({ ok: true, now: Date.now() }), {
-    headers: { "content-type": "application/json" }
-  });
-}
